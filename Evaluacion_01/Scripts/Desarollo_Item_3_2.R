f1d968c7caffc347af68b3218e2bc1eadbc8a1f6
Ana Villagran <akarinavillagrani@gmail.com>
2016-10-22 20:44:23 -0300

    smith-waterman terminado

--- Evaluacion_01/Scripts/Desarollo_Item_3_2.R
+++ Evaluacion_01/Scripts/Desarollo_Item_3_2.R
@@ -1,13 +1,10 @@
-s
-biocLite()
-
-
 library(Biostrings)
 
 # Actividad 3.2 Aliniamiento con R
 
+setwd("C:\\Users\\Kari\\Documents\\GitHub\\Bioinformatica\\Evaluacion_01\\Scripts")
 
-seq1<-DNAString("GAATTCCTACTACGAAGAATTCCTACTACGAAACTACGAAAATTCCTACTACGA")
+seq1<-DNAString("GAATTCCTACTACGAAGAATTCCTACTACGAAACTACGAAAATTCCTACTACGA") 
 seq2<-DNAString("GAATTCCTACTACGGAATTCCCCTCCCATAATTCCTACTACGA")
 
 # Alineamiento Global
@@ -20,5 +17,5 @@ print(globalAlign)
 
 
 mat = nucleotideSubstitutionMatrix(match = 1, mismatch = -1, baseOnly = TRUE)
-globalAlign = pairwiseAlignment(seq1, seq2, type="local", substitutionMatrix = mat,gapOpening=0, gapExtension = -1)
-print(globalAlign)
+localAlign = pairwiseAlignment(seq1, seq2, type="local", substitutionMatrix = mat,gapOpening=0, gapExtension = 2)
+print(localAlign)
--- Evaluacion_01/Scripts/smith-waterman.R
+++ Evaluacion_01/Scripts/smith-waterman.R
@@ -4,19 +4,69 @@ library(stringr)
 
 # Algoritmo Smith-Waterman
 
-prueba <- function (s1,s2)
-{
-  print(0)
-}
 
 smith <- function (s1,s2)
 {
-  matriz = matrix(nrow = nchar(s1) + 1 , ncol = nchar(s2)+1)
+  matriz = matrix(nrow=nchar(s2)+1,ncol=nchar(s1) + 1)
   s1 <- paste("",s1)
   s2 <- paste("",s2)
-  rownames(matriz) <- c(unlist(strsplit(s1, "")))
-  colnames(matriz) <- c(unlist(strsplit(s2, "")))
-  inicializar_matriz(matriz)
+  colnames(matriz) <- c(unlist(strsplit(s1, "")))
+  rownames(matriz) <- c(unlist(strsplit(s2, "")))
+  matriz <- inicializar_matriz(matriz)
+  matriz <- fill_table(matriz)
+  vectorScore <- score(matriz)
+  scoreResult <- paste("El score obtenido es de : ",vectorScore[1])
+  print(scoreResult)
+  #print(matriz)
+  vectorString <- reconstructionString(matriz,vectorScore[2],vectorScore[3])
+  print(vectorString[1])
+  print(vectorString[2])
+}
+
+score <- function(matriz)
+{
+  maxValue <- 0
+  iMAxPos <- 0
+  jMAxPos <- 0
+  for(i in 2:nrow(matriz))
+  {
+    for(j in 2:ncol(matriz))
+    {
+      maxValue<- pmax(maxValue,matriz[i,j])
+      if(maxValue == pmax(maxValue,matriz[i,j]))
+      {
+        iMAxPos <- i
+        jMAxPos <- j
+      }
+    }
+  }
+  
+  return(c(maxValue,iMAxPos,jMAxPos))
+}
+
+
+fill_table <- function(matriz)
+{
+  match <- 1
+  gap <- -2
+  mmatch <- -1
+  puntaje <- 0
+  for(i in 2:nrow(matriz))
+  {
+    for(j in 2:ncol(matriz))
+    {
+      if(rownames(matriz)[i]==colnames(matriz)[j])
+      {
+        puntaje <- highScore(matriz[i-1,j]+gap,matriz[i,j-1]+gap,matriz[i-1,j-1]+match)
+      }
+      else
+      {
+        puntaje <- highScore(matriz[i-1,j]+gap,matriz[i,j-1]+gap,matriz[i-1,j-1]+mmatch)
+      }
+      matriz[i,j] <- puntaje
+    }
+  }
+  return(matriz)
 }
 
 
@@ -30,21 +80,62 @@ inicializar_matriz <- function (matriz)
   {
     matriz[1,j] <- 0
   }
-    print(matriz)
+  return(matriz)
 }
-
-
 
 highScore <- function (v1,v2,v3)
 {
-  pmax(0,v1,v2,v3)
+  return(pmax(0,v1,v2,v3))
+}
+
+reconstructionString <- function(matriz, imax, jmax)
+{
+  s1 <- ""
+  s2 <- ""
+  while((imax > 1 && jmax > 1) && matriz[imax,jmax] > 0 )
+  {
+    if(rownames(matriz)[imax]==colnames(matriz)[jmax])
+    {
+      s1 <- paste(colnames(matriz)[jmax],s1,sep="")
+      s2 <- paste(rownames(matriz)[imax],s2,sep="")
+      imax <- imax-1
+      jmax <- jmax-1
+    }
+    else
+    {
+      if(matriz[imax-1,jmax] > matriz[imax,jmax-1])
+      {
+        s2 <- paste(rownames(matriz)[imax],s2,sep="")
+        s1 <- paste("-",s1,sep="")
+        imax <- imax-1
+      }
+      else
+      {
+        s2 <- paste("-",s2,sep="")
+        s1 <- paste(colnames(matriz)[jmax],s1,sep="")
+        jmax <- jmax-1
+      }
+    }
+  }
+  return(c(s1,s2))
 }
 
 
 
 
+smith('AAAA','AAAA')
 
-smith('hola','hola')
+smith('GAATTCCTACTACGAAGAATTCCTACTACGAAACTACGAAAATTCCTACTACGA',
+      'GAATTCCTACTACGGAATTCCCCTCCCATAATTCCTACTACGA')
+
+matriz = matrix(nrow = nchar("hola") + 1 , ncol = nchar("hola")+1)
+s1 <- paste("","hola")
+s2 <- paste("","hola")
+rownames(matriz) <- c(unlist(strsplit(s1, "")))
+colnames(matriz) <- c(unlist(strsplit(s2, "")))
+matriz
+
+
 
 
 
